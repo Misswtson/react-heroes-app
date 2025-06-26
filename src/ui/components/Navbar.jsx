7ddd861e5dbf214ee4d1cee3e57b4d57d9d73332
Mac <mac@Macs-MacBook-Air.local>
2025-06-26 12:29:19 -0700

    Adding a login page, and creating a second router using usenavigate hook

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,7 +1,12 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const Navbar = () => {
+
+const navigate = useNavigate();
+const onLogout = () => {navigate('/login', {replace : true});
+}
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             
@@ -37,9 +42,7 @@ export const Navbar = () => {
                         Miss Watson
                     </span>
 
-                    <button className='nav-item nav-link btn'>
-Logout
-                    </button>
+<button className='nav-item nav-link btn' onClick={onLogout}> Logout </button>
                 </ul>
             </div>
         </nav>
